ad42e248ea98f7b9b8876702998d492d78cf2e86
allxdxd <asdfnestor@gmail.com>
2023-01-20 19:11:23 -0500

    modified:   src/components/footer/footer.jsx 	modified:   src/components/footer/styles.js

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,6 +4,7 @@ import styles from "./styles.js";
 function Footer() {
   return (
     <View style={{ backgroundColor: "#fff" }}>
+
       <View style={{ flexDirection: "row", marginHorizontal: 40 }}>
         <View style={styles.separator1} />
         <Text style={styles.separator2}>O</Text>
@@ -11,21 +12,11 @@ function Footer() {
       </View>
 
       <View style={styles.create}>
-        <TouchableOpacity
-          style={{ backgroundColor: "#31A34C", height: 35, borderRadius: 2 }}
-        >
-          <Text
-            style={{
-              color: "#fff",
-              textAlign: "center",
-              marginTop: 8.5,
-              fontSize: 15
-            }}
-          >
-            Crear cuenta de Facebook
-          </Text>
+        <TouchableOpacity style={styles.createbtn}>
+          <Text style={styles.textcreatebtn}>Crear cuenta de Facebook</Text>
         </TouchableOpacity>
       </View>
+      
     </View>
   );
 }
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   separator1: {
@@ -7,19 +7,30 @@ const styles = StyleSheet.create({
     flex: 1,
     alignSelf: "center"
   },
-  separator2:{
+  separator2: {
     alignSelf: "center",
     paddingHorizontal: 5,
     fontSize: 15,
     color: "#A7A8AB"
   },
   create: {
-    background: '#fff',
+    background: "#fff",
     marginTop: 20,
     flex: 1,
     padding: 0,
-    marginHorizontal: 60,
+    marginHorizontal: 60
   },
+  createbtn: {
+    backgroundColor: "#31A34C",
+    height: 35,
+    borderRadius: 2
+  },
+  textcreatebtn: {
+    color: "#fff",
+    textAlign: "center",
+    marginTop: 8.5,
+    fontSize: 15
+  }
 });
 
 export default styles;
